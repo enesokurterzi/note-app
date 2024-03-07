84b008a0e17a7a69a17be8b7a7f50cd05a5abf0f
Enes OKURTERZİ <113862251+enesokurterzi@users.noreply.github.com>
2024-03-07 20:01:37 +0300

    ADD Dagger-Hilt(AppModule.kt and NoteApp.kt)

---
+++ app/src/main/java/com/example/noteapp/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.example.noteapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.example.noteapp.feature_note.data.data_source.NoteDataBase
+import com.example.noteapp.feature_note.data.repository.NoteRepositoryImpl
+import com.example.noteapp.feature_note.domain.repository.NoteRepository
+import com.example.noteapp.feature_note.domain.use_case.DeleteNoteUseCase
+import com.example.noteapp.feature_note.domain.use_case.GetNotesUseCase
+import com.example.noteapp.feature_note.domain.use_case.NoteUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNoteDatabase(app: Application): NoteDataBase {
+        return Room.databaseBuilder(
+            app,
+            NoteDataBase::class.java,
+            NoteDataBase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteRepository(db: NoteDataBase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NoteUseCases {
+        return NoteUseCases(
+            getNotesUseCase = GetNotesUseCase(repository),
+            deleteNoteUseCase = DeleteNoteUseCase(repository)
+        )
+    }
+
+}
