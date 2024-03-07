461274d9117ff690b258a27783fae568be876255
Enes OKURTERZİ <113862251+enesokurterzi@users.noreply.github.com>
2024-03-07 19:47:17 +0300

    ADD DeleteNoteUseCase.kt

---
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/DeleteNoteUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.noteapp.feature_note.domain.use_case
+
+import com.example.noteapp.feature_note.domain.model.Note
+import com.example.noteapp.feature_note.domain.repository.NoteRepository
+
+class DeleteNoteUseCase(
+    private val repository: NoteRepository
+) {
+    suspend operator fun invoke(note: Note) {
+        repository.deleteNote(note)
+    }
+}
