cc429196a488768f2379cdae6c19fca30a1ad740
Enes OKURTERZİ <113862251+enesokurterzi@users.noreply.github.com>
2024-03-09 14:01:32 +0300

    ADD GetNoteUseCase.kt

---
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/GetNoteUseCase.kt
@@ -0,0 +1,12 @@
+package com.example.noteapp.feature_note.domain.use_case
+
+import com.example.noteapp.feature_note.domain.model.Note
+import com.example.noteapp.feature_note.domain.repository.NoteRepository
+
+class GetNoteUseCase(
+    private val repository: NoteRepository
+) {
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+}
