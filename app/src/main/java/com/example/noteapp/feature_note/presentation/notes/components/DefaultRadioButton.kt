f9d2941743c87855f7bfc0842c2b873511a745d7
Enes OKURTERZİ <113862251+enesokurterzi@users.noreply.github.com>
2024-03-11 16:13:05 +0300

    UPDATE DefaultRadioButton.kt: entire row is clickable now.

--- app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/notes/components/DefaultRadioButton.kt
@@ -1,5 +1,7 @@
 package com.example.noteapp.feature_note.presentation.notes.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -8,6 +10,7 @@ import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -19,18 +22,28 @@ fun DefaultRadioButton(
     onSelect: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val mutableInteractionSource = remember { MutableInteractionSource() }
+
     Row(
-        modifier = modifier,
+        modifier = modifier
+            .clickable (
+                indication = null,
+                onClick = onSelect,
+                interactionSource = mutableInteractionSource
+            ),
         verticalAlignment = Alignment.CenterVertically
     ) {
         RadioButton(
-            modifier = Modifier.padding(8.dp).size(20.dp),
+            modifier = Modifier
+                .padding(8.dp)
+                .size(20.dp),
             selected = selected,
-            onClick = onSelect,
             colors = RadioButtonDefaults.colors(
                 selectedColor = MaterialTheme.colorScheme.primary,
                 unselectedColor = MaterialTheme.colorScheme.onBackground
-            )
+            ),
+            onClick = onSelect,
+            interactionSource = mutableInteractionSource
         )
         Text(text = text, style = MaterialTheme.typography.bodySmall)
     }
