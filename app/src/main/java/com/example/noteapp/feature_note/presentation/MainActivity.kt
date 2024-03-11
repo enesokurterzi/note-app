f0d8a00ccae0c385bac5c5d83d854e275b295b96
Enes OKURTERZİ <113862251+enesokurterzi@users.noreply.github.com>
2024-03-11 22:39:39 +0300

    UPDATE Unused imports removed: MainActivity.kt

--- app/src/main/java/com/example/noteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/MainActivity.kt
@@ -5,15 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import com.example.noteapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
-import com.example.noteapp.feature_note.presentation.notes.NotesScreen
 import com.example.noteapp.feature_note.presentation.util.Navigation
-import com.example.noteapp.feature_note.presentation.util.Screen
 import com.example.noteapp.ui.theme.NoteAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
